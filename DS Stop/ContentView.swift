4dc8ad7ea45795147a73f2e6dc4ac8038085eb9e
Acan <110075452+Zeyu-Xie@users.noreply.github.com>
2024-12-09 12:58:42 +0100

    .

--- DS Stop/ContentView.swift
+++ DS Stop/ContentView.swift
@@ -12,17 +12,15 @@ struct ContentView: View {
     @State private var output: Array<String> = []
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            List(output, id: \.self) { item in
-                Text(item)
+        NavigationView() {
+            List {
+                NavigationLink(destination: Text("Dealing with .DS_Store")) {
+                    Label(".DS_Store", systemImage: "globe")
+                }
             }
-            .padding()
+            .listStyle(SidebarListStyle())
+            .navigationTitle("DS Stop")
         }
-        .padding()
         .onAppear() {
             output = getSpecifiedFile(
                 fromDirectory: "/Users/zeyuxie/Downloads/",
